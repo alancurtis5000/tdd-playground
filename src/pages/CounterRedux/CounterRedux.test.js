1369c3f74fadd41e9bb8892a872c49abdfef61d7
Alan Curtis <alancurtis5000@gmail.com>
2020-10-04 11:31:35 -0600

    counterRedux test fail

--- src/pages/CounterRedux/CounterRedux.test.js
+++ src/pages/CounterRedux/CounterRedux.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
 describe('CounterRedux', () => {
+  afterEach(cleanup);
+
   test('placeholder test', () => {
     expect(true).toEqual(true);
+  });
+  it('should render page', () => {
+    // const { getByTestId } = render(
+    //     <ACounter />
+    // );
+    // expect(getByTestId('count')).toHaveTextContent('3');
+    expect(true).toEqual(false);
   });
 });
