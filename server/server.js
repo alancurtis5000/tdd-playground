bd0bf43ab26ae119c4e0ac225ceb874dbf6ab273
Alan Curtis <alancurtis5000@gmail.com>
2020-10-04 20:34:06 -0600

    added basic functionality of axios get

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const random = require('lodash/random');
 
 const app = express();
 
@@ -7,12 +8,20 @@ const app = express();
 app.use(express.static(`${__dirname}/../build`));
 app.use(bodyParser.json());
 
+const quotes = [
+  'good times are here',
+  'cheese is the food of mice',
+  'dancing is the best',
+  'retreat if you are a coward',
+];
+
 // Endpoints
 app.get('/api/test', (req, res) => {
   console.log('hit server');
+  const result = quotes[random(0, 3)];
   setTimeout(() => {
-    res.status(200).send('hello');
-  }, 2000);
+    res.status(200).send(result);
+  }, 000);
 });
 
 // Launch Server
--- src/components/Fetch01.js
+++ src/components/Fetch01.js
@@ -2,20 +2,44 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Fetch01 = () => {
-  const [isPending, setIsPending] = useState(true);
+  const [isPending, setIsPending] = useState(false);
   const [quote, setQuote] = useState(' default ');
+  const [error, setError] = useState('');
 
   const getQuote = () => {
     console.log('getQuote triggered');
-    axios.get('/api/test').then((res) => {
-      console.log('res: ', res);
-    });
+    setIsPending(true);
+    setQuote('...');
+    setError('');
+    axios
+      .get('/api/test')
+      .then((res) => {
+        console.log('res: ', res);
+        setIsPending(false);
+        setQuote(res.data);
+      })
+      .catch((err) => {
+        console.log('Error:', err);
+        console.log('more', err.message);
+        setIsPending(false);
+        setError(err.message);
+      });
+  };
+
+  const renderLoading = () => {
+    return <div>...loading...</div>;
+  };
+
+  const renderError = () => {
+    return <div>{error}</div>;
   };
 
   return (
     <div className="Fetch01">
-      {quote}
       <button onClick={getQuote}>Get Quote</button>
+      {quote}
+      {isPending ? renderLoading() : null}
+      {error ? renderError() : null}
     </div>
   );
 };
